4d81f29b6a71a3d9f973fa60e9c524802b166ab8
André Meireles <andre2meireles@gmail.com>
2024-02-19 23:23:45 -0300

    Improve debug log

--- container_builder.go
+++ container_builder.go
@@ -7,16 +7,17 @@ import (
 )
 
 type buildResult struct {
+	container    *Container
 	hasError     bool
 	errorMessage string
 }
 
 func fail(message string) buildResult {
-	return buildResult{hasError: true, errorMessage: message}
+	return buildResult{hasError: true, errorMessage: message, container: nil}
 }
 
-func ok() buildResult {
-	return buildResult{hasError: false, errorMessage: ""}
+func ok(container *Container) buildResult {
+	return buildResult{container: container, hasError: false, errorMessage: ""}
 }
 
 type containerBuilder struct {
@@ -32,6 +33,7 @@ type containerBuilder struct {
 //   - dependency ([]Dependency) slice of dependencies
 //   - timeout (*float64) time in milliseconds to define a deadline to container build, if is nil, it will be 2 minutes
 //   - recursion (*int) max number of times that builder function must execute before panic, if is nil, it will be 1000 times
+//   - debug (bool) if is true, it will print debug messages
 func NewContainerBuilder(
 	dependency []Dependency,
 	timeout *float64,
@@ -52,7 +54,7 @@ func NewContainerBuilder(
 		dependencies: dependency,
 		timeout:      &deadline,
 		recursion:    &rec,
-		debug:	      debug,
+		debug:        debug,
 	}
 }
 
@@ -63,18 +65,20 @@ func (db containerBuilder) Build() *Container {
 	duration := time.Duration(*db.timeout) * time.Millisecond
 	deadline := time.Now().Add(duration)
 	ctx, cancelFunc := context.WithDeadline(context.Background(), deadline)
+
 	defer cancelFunc()
 
-	containerChannel := make(chan *Container, 1)
-	errorChannel := make(chan buildResult, 1)
-	go db.resolveDependencies(ctx, recursion, deps, errorChannel, containerChannel)
-	build := <-errorChannel
+	resultChannel := make(chan buildResult, 1)
+
+	go db.resolveDependencies(ctx, recursion, deps, resultChannel)
+
+	build := <-resultChannel
 
 	if build.hasError {
 		panic(build.errorMessage)
 	}
 
-	return <-containerChannel
+	return build.container
 }
 
 // Resolve multiple dependencies
@@ -89,61 +93,74 @@ func (db containerBuilder) resolveDependencies(
 	ctx context.Context,
 	maxRecursion int,
 	dependencies []Dependency,
-	errorChannel chan buildResult,
-	containerChannel chan *Container,
+	resultChannel chan buildResult,
 ) {
 	container := new(Container)
 	numberOfRecursions := 0
 	deps := dependencies
 	index := 0
 	maxIndex := len(dependencies)
-	// for _, dep := range dependencies {
+
 	for len(deps) != 0 {
 		if maxIndex == 1 {
-			err := container.Add(deps[0])
-			if err != nil {
-				containerChannel <- nil
-				errorChannel <- fail(fmt.Sprintf("Cannot build container. Last dependency cannot be build, err %s", err))
+			if err := container.Add(deps[0]); err != nil {
+				resultChannel <- fail(fmt.Sprintf("Cannot build container. Last dependency cannot be build, err %s", err))
+
 				return
 			}
-			break
 		}
 
-		// reset index
+		// reset index when reach last position and not been completed
 		if index > maxIndex-1 {
 			index = 0
 		}
 
 		select {
 		case <-ctx.Done():
-			containerChannel <- nil
-			errorChannel <- fail("timeout")
+			resultChannel <- fail("timeout")
+
 			return
 		default:
+			if db.debug {
+				fmt.Printf("Resolving %s\n", deps[index].Name())
+			}
+
 			if numberOfRecursions >= maxRecursion {
-				containerChannel <- nil
-				errorChannel <- fail("max recursion reached")
+				resultChannel <- fail("max recursion reached")
+
 				return
 			}
 
 			err := container.Add(deps[index])
 
-			if db.debug {
-				fmt.Println(err)
+			if err != nil && db.debug {
+				fmt.Printf("Cannot build %s, err %s\n", deps[index].Name(), err)
 			}
 
 			numberOfRecursions++
 
-			// remove existing index, when succeejded
+			// remove existing index, when succeeded
 			if err == nil {
+				if db.debug {
+					fmt.Printf("Added %s\n", deps[index].Name())
+				}
+
 				deps = append(deps[:index], deps[index+1:]...)
 				maxIndex--
+				index = 0
+
+				continue
+			}
+
+			if db.debug {
+				for _, d := range deps {
+					fmt.Println("Must resolve:", d.Name())
+				}
 			}
 
 			index++
 		}
 	}
 
-	errorChannel <- ok()
-	containerChannel <- container
+	resultChannel <- ok(container)
 }
